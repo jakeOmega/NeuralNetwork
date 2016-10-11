9abecddebd38930e5666fcd5135a6f3d696612d8
jakeOmega <jakefishmaui@gmail.com>
2016-10-11 12:24:35 -0600

    Updated comments a bit
    Put comments in the mainForm part, as well as in the actual neural
    network class part

--- neuralNet/neuralNet/mainForm.cs
+++ neuralNet/neuralNet/mainForm.cs
@@ -81,14 +81,17 @@ namespace neuralNet
 
         ///<summary>
         ///For a given neural network with one output and a list of files that should be given
-        ///either an output of 1 (goodFiles) or -1 (badFiles)
+        ///either an output of 1 (goodFiles) or 0 (badFiles), and 
         ///</summary>
         private double trainingError(net netIn, List<string> goodFiles, List<string> badFiles)
         {
+            //count how many the net get right, wrong, or uncertain
+            //print the wrong and uncertain ones
             double error = 0;
             int right = 0;
             int uncertain = 0;
             int wrong = 0;
+            //All the files that should output 1
             for (int i = 0; i < goodFiles.Count; i++)
             {
                 Vector<double> input = fileToInput(goodFiles[i]);
@@ -111,6 +114,7 @@ namespace neuralNet
                 }
                 error += netIn.error(input, output);
             }
+            //all the files that should output 0
             for (int i = 0; i < badFiles.Count; i++)
             {
                 Vector<double> input = fileToInput(badFiles[i]);
@@ -137,52 +141,70 @@ namespace neuralNet
             return error / (goodFiles.Count + badFiles.Count);
         }
 
+        ///<summary>
+        ///Train a neural network on a bunch of images to distinguish between two categories of images
+        ///(currently EU4 screenshots versus everything else, as that's what I had images of available)
+        ///with a random selection of images as testing images. The network doesn't train with these, and so
+        ///they provide a good measure of the accuracy of the network
+        ///</summary>
         public void pictureNetTrain()
         {
             int testCountPerCat = 30;
             Random rnd = new Random();
+            //Grab all the good (output = 1) and bad (output = 0) images and put them in a list in random order
             List<string> goodFilesIn = System.IO.Directory.GetFiles("D:\\Libraries\\Desktop\\images\\good").ToList();
             List<string> goodFiles = new List<string>(goodFilesIn.OrderBy(x => rnd.Next()).ToArray());
+            //The last testCountPerCat images in the goodFiles list will be used as test images; not trained against
             int goodFileCount = goodFiles.Count() - testCountPerCat;
             List<string> goodFilesTrain = goodFiles.GetRange(0, goodFileCount);
             List<string> goodFilesTest = goodFiles.GetRange(goodFileCount, testCountPerCat);
             List<string> badFilesIn = System.IO.Directory.GetFiles("D:\\Libraries\\Desktop\\images\\bad").ToList();
             List<string> badFiles = new List<string>(badFilesIn.OrderBy(x => rnd.Next()).ToArray());
+            //same idea as for good files
             int badFileCount = badFiles.Count() - testCountPerCat;
             List<string> badFilesTrain = badFiles.GetRange(0, badFileCount);
             List<string> badFilesTest = badFiles.GetRange(badFileCount, testCountPerCat);
+            //Make a list of the inputs and outputs, excluding the testing images
             List<Vector<double>> inputs = new List<Vector<double>>();
             List<Vector<double>> outputs = new List<Vector<double>>();
-            for (int j = 0; j < goodFileCount + badFileCount; j++)
+            for (int j = 0; j < goodFileCount; j++) //only goes up to goodFileCount - therefore excludes test images
             {
                 string file;
                 Vector<double> output = Vector<double>.Build.Dense(1);
-                if (j >= goodFileCount)
-                {
-                    file = badFiles[j - goodFileCount];
-                    output[0] = 0;
-                }
-                else
-                {
-                    file = goodFiles[j];
-                    output[0] = 1;
-                }
+                file = goodFiles[j];
+                output[0] = 1;
 
                 Vector<double> input = fileToInput(file);
                 inputs.Add(input);
                 outputs.Add(output);
             }
 
+            for (int j = 0; j < badFileCount; j++)
+            {
+                string file;
+                Vector<double> output = Vector<double>.Build.Dense(1);
+                file = badFiles[j];
+                output[0] = 0;
+
+                Vector<double> input = fileToInput(file);
+                inputs.Add(input);
+                outputs.Add(output);
+            }
+
+            //a list of locations in the input/output lists, will be randomized to draw from to train
             int[] intRange = Enumerable.Range(0, goodFileCount + badFileCount).ToArray();
 
-            //net net = new net("D:\\Libraries\\Documents\\tempTest\\net4.net");
-            net net = new net(4*pictureWidth*pictureHeight, 1, 2, 30);
+            //Load in an old net or create a new one
+            net net = new net("D:\\Libraries\\Documents\\tempTest\\net4.net");
+            //net net = new net(4*pictureWidth*pictureHeight, 1, 2, 30);
 
-
-            double priorError = 1000;
+            //We'll only save the net to file when it's error on the test images decreases
+            double priorError = 1;
             int trainNum = 0;
+            //Until the user closes the training thing
             while (true)
             {
+                //randomly grab 10 inputs and their corresponding outputs
                 int[] imageID = intRange.OrderBy(x => rnd.Next()).ToArray();
                 List<Vector<double>> inputsForRound = new List<Vector<double>>();
                 List<Vector<double>> outputsForRound = new List<Vector<double>>();
@@ -191,7 +213,9 @@ namespace neuralNet
                     inputsForRound.Add(inputs[imageID[j]]);
                     outputsForRound.Add(outputs[imageID[j]]);
                 }
+                //train the net with the selected inputs and outputs
                 net.train(inputsForRound, outputsForRound, 1);
+                //every once in a while, check how we're doing, record it on the graph, and save if we're improving
                 if (trainNum % 100 == 0)
                 {
                     double error = trainingError(net, goodFilesTest, badFilesTest);
@@ -208,6 +232,11 @@ namespace neuralNet
             }
         }
 
+        ///<summary>
+        ///Adds a point to the error chart, supports multithreading so
+        ///we can add a point to the chart without lag when the training is 
+        ///fully using a CPU core
+        ///</summary>
         private void addPoint(int index, double error, int series)
         {
             // InvokeRequired required compares the thread ID of the
@@ -224,6 +253,7 @@ namespace neuralNet
             }
         }
 
+        //Update the chart every so often
         public void updateChart(Object o, EventArgs e)
         {
             errorChart.Update();
